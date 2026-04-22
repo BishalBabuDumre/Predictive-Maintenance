d643c5c26f25bcfe4e7f420460be79656c0ed033
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-04-22 15:14:43 -0700

    Update isolationForest.py

--- training/isolationForest.py
+++ training/isolationForest.py
@@ -5,11 +5,9 @@ from sklearn.ensemble import IsolationForest
 import time
 
 t0 = time.time()
-# Get the directory where the script is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# Combine it with the filename
-file_path = os.path.join(script_dir, 'benLomond.csv')
-save_path = os.path.join(script_dir, 'anomalies_0.csv')
+
+file_path = os.path.join('./../data/raw/original.csv')
+save_path = os.path.join('./../data/processed/anomalies_0.csv')
 
 df = pd.read_csv(file_path, parse_dates=["DateTime"])
 df = df.sort_values("DateTime").reset_index(drop=True)
