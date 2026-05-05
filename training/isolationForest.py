a0b51aaf808e558ae4be8a2f08e1d1e89826d596
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-05-04 20:13:49 -0700

    Refactor Isolation Forest code with consistent formatting

--- training/isolationForest.py
+++ training/isolationForest.py
@@ -77,7 +77,7 @@ X = df[features]
 model = IsolationForest(
     n_estimators=88,
     max_samples=0.96,
-    max_features: 12,
+    max_features=12,
     random_state=42,
     contamination=0.0015
 )
