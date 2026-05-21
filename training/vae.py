bbf009c42003c74d295ebe576ff2bfeef2758f54
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-05-21 14:59:15 -0700

    Update vae.py

--- training/vae.py
+++ training/vae.py
@@ -1,51 +1,14 @@
 import os
 import torch
-import torch.nn as nn
 import torch.optim as optim
+from training.model import VAE
+from training.model import vae_loss_function
 from training.data_preparation import prepare_vae_data
 from training.feature_engineering import prepare_data_frame
 from training.early_stopping import EarlyStopping
 from training.onnx_export import export_and_verify_onnx
 
 file_path = os.path.join('data/raw/original.csv')
-
-class VAE(nn.Module):
-    def __init__(self, input_dim, latent_dim=4):
-        super(VAE, self).__init__()
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU()
-        )
-        self.fc_mu = nn.Linear(16, latent_dim)
-        self.fc_logvar = nn.Linear(16, latent_dim)
-        
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 16),
-            nn.ReLU(),
-            nn.Linear(16, 32),
-            nn.ReLU(),
-            nn.Linear(32, input_dim)
-        )
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
-
-    def forward(self, x):
-        h = self.encoder(x)
-        mu, logvar = self.fc_mu(h), self.fc_logvar(h)
-        z = self.reparameterize(mu, logvar)
-        return self.decoder(z), mu, logvar
-
-def vae_loss_function(recon_x, x, mu, logvar):
-    MSE = nn.functional.mse_loss(recon_x, x, reduction='sum')
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return MSE + KLD
 
 # Usage
 df, features, target = prepare_data_frame(file_path)
