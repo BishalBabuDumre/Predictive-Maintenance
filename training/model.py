76b669485584009dfa9ce1bb9c635912fcf744f4
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-05-28 11:47:37 -0700

    Update model.py

--- training/model.py
+++ training/model.py
@@ -15,8 +15,8 @@ class VAE(nn.Module):
         # --- Dynamic Encoder Construction ---
         encoder_modules = []
         current_dim = input_dim
-        for h_dim in hidden_layers.split(","):
-            encoder_modules.append(nn.Linear(current_dim, int(h_dim)))
+        for h_dim in hidden_layers:
+            encoder_modules.append(nn.Linear(current_dim, h_dim))
             encoder_modules.append(act_fn())
             # NEW: Add dropout if configured
             if dropout is not None:
