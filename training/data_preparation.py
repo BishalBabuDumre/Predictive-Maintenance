f0dba2fc787c2fb2a99ec9240a91ece5045bc096
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-05-29 15:36:20 -0700

    Update data_preparation.py

--- training/data_preparation.py
+++ training/data_preparation.py
@@ -1,8 +1,10 @@
 import torch
+import joblib
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import DataLoader, TensorDataset
+from training.save_file import save_filename
 
-def prepare_vae_data(df, features, target, batch_size=64):
+def prepare_vae_data(df, features, target, scaler_x_name, scaler_y_name, batch_size=64):
     
     X = df[features].to_numpy()
     y = df[target].to_numpy()
@@ -11,6 +13,12 @@ def prepare_vae_data(df, features, target, batch_size=64):
     # VAEs perform best when inputs are bounded (0,1) or (-1,1)
     scaler_x = MinMaxScaler(feature_range=(-1, 1))
     scaler_y = MinMaxScaler(feature_range=(0, 1))
+
+    if scaler_x_name:
+        save_filename(scaler_x, scaler_x_name)
+
+    if scaler_y_name:
+        save_filename(scaler_y, scaler_y_name)
     
     X_scaled = scaler_x.fit_transform(X)
     y_scaled = scaler_y.fit_transform(y)
