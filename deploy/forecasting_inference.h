e39303d496e9d351d957919441fb53a01e43bb31
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-06-19 16:46:57 -0700

    Create forecasting_inference.h

---
+++ deploy/forecasting_inference.h
@@ -0,0 +1,21 @@
+#define FORECASTING_INFERENCE_H
+
+#include <string>
+#include <vector>
+#include "onnx_engine.h"
+#include "inference_handler.h"
+
+class ForecastingInference {
+public:
+    ForecastingInference(const std::string& model_path);
+    ~ForecastingInference() = default;
+
+    // Evaluates an incoming vector and returns structured diagnostic logs
+    InferenceResult analyzeSensorState(const std::vector<float>& feature_vector);
+
+private:
+    OnnxEngine model_engine_;
+    InferenceHandler decision_handler_;
+};
+
+#endif // FORECASTING_INFERENCE_H
