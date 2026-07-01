ea2ff9a560580ce6289fd074062f5e0d5561c480
Bishal Babu Dumre <bishalbabud130@gmail.com>
2026-07-01 15:47:17 -0700

    Remove computeRollingStats from feature_extractor.h
    Removed the computeRollingStats function declaration.

--- deploy/feature_extractor.h
+++ deploy/feature_extractor.h
@@ -13,7 +13,4 @@ public:
 private:
     // Helper to get a past element relative to the newest write position
     float getPastReading(const float* buffer, int index_ptr, int steps_back) const;
-
-    // Rolling windows engine calculation
-    void computeRollingStats(const float* buffer, int index_ptr, int window_size, float& mean, float& std) const;
 };
